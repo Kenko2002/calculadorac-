b06483d9dcd0368a0edcac9efa023b44c9b7766d
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2024-02-29 09:31:58 -0300

    Delete testes directory

--- testes/src/controller/ControladorCalculadora.cs
+++
@@ -1,9 +0,0 @@
-public class ControladorCalculadora {
-
-  public int calcular(String operacao, int a, int b) {
-
-    Calculadora calc = new Calculadora();
-    return calc.calcular(operacao, a, b);
-  }
-
-}
--- testes/src/model/contas/Divisao.cs
+++
@@ -1,9 +0,0 @@
-public class Divisao {
-    
-    public int calcular(int a, int b){
-        if (b == 0){ throw new DivideByZeroException("Não é possível dividir por zero."); }
-        int resultado=a/b;
-
-        return resultado;
-    }
-}
